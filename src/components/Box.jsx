deb7f4ca31cd9c70ed34b14bf7c330ccbc187530
Matheus Martini <matheus.martini@rede.ulbra.br>
2023-03-15 16:39:35 -0300

    add texture

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,35 @@
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+const name = (type) => `boxMaterial/WoodFloor041_2K_${type}.jpg`;
+
 function Box(props){
+
+    const [
+        colorMap,
+        displacementMap,
+        normalMap,
+        roughnessMap,
+        aoMap
+    ] = useLoader(TextureLoader, [
+        name("Color"),
+        name("Displacement"),
+        name("NormalGL"),
+        name("Roughness"),
+        name("AmbientOcclusion")
+    ]);
 
     return(
         <mesh {...props} recieveShadow={true} castShadow>
             <boxBufferGeometry/>
-            <meshPhysicalMaterial color={'blue'}/>
+            <meshPhysicalMaterial 
+                displacementScale={0}
+                map={colorMap}
+                displacementMap={displacementMap}
+                normalMap={normalMap}
+                roughnessMap={roughnessMap}
+                aoMap={aoMap}
+            />
         </mesh>
     )
 
--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -1,9 +1,36 @@
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+const name = (type) => `floorMaterial/PavingStones035_1K_${type}.jpg`;
+
 function Floor(props){
+
+    const [
+        colorMap,
+        displacementMap,
+        normalMap,
+        roughnessMap,
+        aoMap
+    ] = useLoader(TextureLoader, [
+        name("Color"),
+        name("Displacement"),
+        name("NormalGL"),
+        name("Roughness"),
+        name("AmbientOcclusion")
+    ]);
+
 
     return(
         <mesh {...props} recieveShadow>
             <boxBufferGeometry args={[10, 1, 10]}/>
-            <meshPhysicalMaterial color={'red'}/>
+            <meshPhysicalMaterial 
+                displacementScale={0}
+                map={colorMap}
+                displacementMap={displacementMap}
+                normalMap={normalMap}
+                roughnessMap={roughnessMap}
+                aoMap={aoMap}    
+            />
         </mesh>
     )
 
