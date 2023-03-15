e8a49fae00c05d7d1919a5efdc7dbe04cadf650c
Matheus Martini <matheus.martini@rede.ulbra.br>
2023-03-15 16:40:16 -0300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic'
 import css from '../styles/Home.module.css'
 import {Canvas} from '@react-three/fiber'
 
@@ -6,6 +7,17 @@ import Box from '@/components/Box'
 import Controls from '@/components/OrbitControls'
 import Draggable from '@/components/Draggable'
 import Light from '@/components/Light'
+import ModelViewer from '@/components/ModelViewer'
+
+const Barrel = dynamic(() => import('../components/ModelViewer'), { ssr: false })
+
+const Barrels = () => {
+  return (
+    <Barrel
+      modelPath={`../modelsGlb/construction_barrel_-_3d_scan_quixel_megascans.glb`}
+    />
+  )
+}
 
 export default function Home(){
   return (
@@ -14,16 +26,16 @@ export default function Home(){
         position: [-6, 7, 7],
       }}>
 
-        <ambientLight color={'white'} intensity={0.4}/>
+        <ambientLight color={'white'} intensity={1}/>
         <Light position={[0, 3, 0]}/>
 
         <Draggable>
           <Box/>
           <Floor position={[0, -1, 0]}/>
         </Draggable>
-        
         <Controls />
-
+        {/* <Barrels/> */}
+        {/* <ModelViewer scale='40' modelPath={'../modelsGlb/construction_barrel_-_3d_scan_quixel_megascans.glb'}/> */}
       </Canvas>
     </div>
   )
